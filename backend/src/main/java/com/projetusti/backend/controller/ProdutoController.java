8f5f81189c3677fbe71983773cac3d3cdd7a0770
Jadson <oi@evotax.com>
2025-09-06 00:16:25 -0300

    feat: implementacao do metodo listar e configuracao do CORS

--- backend/src/main/java/com/projetusti/backend/controller/ProdutoController.java
+++ backend/src/main/java/com/projetusti/backend/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import com.projetusti.backend.model.Produto;
 import com.projetusti.backend.service.ProdutoService;
 
 @RestController
+// Libera o acesso do localhost:3000 para TODOS os endpoints deste controller
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/produtos")
 public class ProdutoController {
  private final ProdutoService service;
---
+++ frontend/src/components/Saudacao.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Saudacao(props) {
+  return (
+    <h1> Holla {props.nome} {props.idade} anos</h1>
+  )
+}
+
+export default Saudacao
